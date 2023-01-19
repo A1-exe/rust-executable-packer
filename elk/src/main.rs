b9fc8bc66612f03840b28a6c55bf61cdd0f1fc49
A1 exe <tspears2@hawk.iit.edu>
2023-01-18 18:10:25 -0600

    Add ndisasm function

--- elk/src/main.rs
+++ elk/src/main.rs
@@ -6,11 +6,44 @@ fn main() -> Result<(), Box<dyn Error>> {
   let input_path = env::args().nth(1).expect("usage: elk FILE");
   let input = fs::read(&input_path)?;
   
-  let file = match delf::File::parse_or_print_error(&input[..]) {
-    Some(f) => f,
-    None => std::process::exit(1),
+  // println!("Analyzing {}...", input_path);
+  // let file = match delf::File::parse_or_print_error(&input[..]) {
+  //   Some(f) => f,
+  //   None => std::process::exit(1),
+  // };
+  // println!("{:#?}", file);
+
+  // println!("Executing {:?}...", input_path);
+  // use std::process::Command;
+  // let status = Command::new(input_path).status()?;
+  // if !status.success() {
+  //   return Err("process did not exit successfully".into());
+  // }
+
+  println!("Disassembling {:?}...", input_path);
+  let code = &input[0x1000..]; // File code offset 0x1000
+  let code = &code[..std::cmp::min(0x25, code.len())]; // Disass at most 0x25 bytes
+  ndisasm(code)?;
+
+  Ok(())
+}
+
+fn ndisasm(code: &[u8]) -> Result<(), Box<dyn Error>> {
+  use std::{
+    io::Write,
+    process::{Command, Stdio},
   };
-  println!("{:#?}", file);
+
+  let mut child = Command::new("ndisasm")
+        .arg("-b")
+        .arg("64")
+        .arg("-")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?;
+    child.stdin.as_mut().unwrap().write_all(code)?;
+  let output = child.wait_with_output()?;
+  println!("{}", String::from_utf8_lossy(&output.stdout));
 
   Ok(())
 }
