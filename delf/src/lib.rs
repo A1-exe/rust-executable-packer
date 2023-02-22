052c8eacc8ea67c34e6a69388f0cb652b1243710
A1 exe <tspears2@hawk.iit.edu>
2023-02-22 01:39:28 -0600

    Part 4 Parse and Apply Rela Relocations

--- delf/src/lib.rs
+++ delf/src/lib.rs
@@ -6,6 +6,50 @@ use std::convert::TryFrom;
 use derive_try_from_primitive::TryFromPrimitive;
 use derive_more::*;
 
+#[derive(thiserror::Error, Debug)]
+pub enum ReadRelaError {
+  #[error("Rela dynamic entry not found")]
+  RelaNotFound,
+  #[error("RelaSz dynamic entry not found")]
+  RelaSzNotFound,
+  #[error("Rela segment not found")]
+  RelaSegmentNotFound,
+  #[error("Parsing error")]
+  ParsingError(nom::error::VerboseErrorKind),
+}
+
+#[derive(Debug, TryFromPrimitive, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum RelType {
+    GlobDat = 6,
+    JumpSlot = 7,
+    Relative = 8,
+}
+impl_parse_for_enum!(RelType, le_u32);
+
+#[derive(Debug)]
+pub struct Rela {
+    pub offset: Addr,
+    pub r#type: RelType,
+    pub sym: u32,
+    pub addend: Addr,
+}
+
+impl Rela {
+  pub fn parse(i: parse::Input) -> parse::Result<Self> {
+    use nom::{combinator::map, number::complete::le_u32};
+    map(
+      tuple((Addr::parse, RelType::parse, le_u32, Addr::parse)),
+      |(offset, r#type, sym, addend)| Self {
+        offset,
+        r#type,
+        sym,
+        addend,
+      },
+    )(i)
+  }
+}
+
 #[derive(Debug)]
 pub struct File {
   pub r#type: Type,
@@ -90,6 +134,53 @@ impl File {
       Err(_) => panic!("unexpected nom error"),
     }
   }
+
+  pub fn segment_at(&self, addr: Addr) -> Option<&ProgramHeader> {
+    self.program_headers
+      .iter()
+      .filter(|ph| ph.r#type == SegmentType::Load)
+      .find(|ph| ph.mem_range().contains(&addr))
+  }
+
+  pub fn segment_of_type(&self, r#type: SegmentType) -> Option<&ProgramHeader> {
+    self.program_headers.iter().find(|ph| ph.r#type == r#type)
+  }
+
+  pub fn dynamic_entry(&self, tag: DynamicTag) -> Option<Addr> {
+    match self.segment_of_type(SegmentType::Dynamic) {
+      Some(ProgramHeader {
+        contents: SegmentContents::Dynamic(entries),
+        ..  // this is a struct pattern, we don't care about the other fields
+      }) => entries.iter().find(|e| e.tag == tag).map(|e| e.addr),
+      _ => None,
+    }
+  }
+
+  pub fn read_rela_entries(&self) -> Result<Vec<Rela>, ReadRelaError> {
+    // see https://blog.rust-lang.org/2019/08/15/Rust-1.37.0.html#referring-to-enum-variants-through-type-aliases
+    use DynamicTag as DT;
+    use ReadRelaError as E;
+
+    // converting `Option<T>` to `Result<T, E>` so we can use `?` to
+    // bubble up errors.
+    let addr = self.dynamic_entry(DT::Rela).ok_or(E::RelaNotFound)?;
+    let len = self.dynamic_entry(DT::RelaSz).ok_or(E::RelaSzNotFound)?;
+    let seg = self.segment_at(addr).ok_or(E::RelaSzNotFound)?;
+
+    // double-slicing trick
+    let i = &seg.data[(addr - seg.mem_range().start).into()..][..len.into()];
+
+    use nom::multi::many0;
+    match many0(Rela::parse)(i) {
+      Ok((_, rela_entries)) => Ok(rela_entries),
+      Err(nom::Err::Failure(err)) | Err(nom::Err::Error(err)) => {
+        let e = &err.errors[0];
+        let (_input, error_kid) = e;
+        Err(E::ParsingError(error_kid.clone()))
+      },
+      _ => unreachable!(),
+    }
+  }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, TryFromPrimitive)]
@@ -134,6 +225,7 @@ pub enum SegmentType {
 impl_parse_for_enum!(SegmentType, le_u32);
 
 use enumflags2::*;
+use nom::sequence::tuple;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, BitFlags)]
 #[repr(u32)]
@@ -143,6 +235,68 @@ pub enum SegmentFlag {
   Read = 0x4,
 }
 impl_parse_for_enumflags!(SegmentFlag, le_u32);
+
+#[derive(Debug, TryFromPrimitive, PartialEq, Eq)]
+#[repr(u64)]
+pub enum DynamicTag {
+  Null = 0,
+  Needed = 1,
+  PltRelSz = 2,
+  PltGot = 3,
+  Hash = 4,
+  StrTab = 5,
+  SymTab = 6,
+  Rela = 7,
+  RelaSz = 8,
+  RelaEnt = 9,
+  StrSz = 10,
+  SymEnt = 11,
+  Init = 12,
+  Fini = 13,
+  SoName = 14,
+  RPath = 15,
+  Symbolic = 16,
+  Rel = 17,
+  RelSz = 18,
+  RelEnt = 19,
+  PltRel = 20,
+  Debug = 21,
+  TextRel = 22,
+  JmpRel = 23,
+  BindNow = 24,
+  InitArray = 25,
+  FiniArray = 26,
+  InitArraySz = 27,
+  FiniArraySz = 28,
+  LoOs = 0x60000000,
+  HiOs = 0x6fffffff,
+  LoProc = 0x70000000,
+  HiProc = 0x7fffffff,
+  GnuHash = 0x6ffffef5,
+  Flags =  0x0000001e,
+  Flags1 = 0x6ffffffb,
+  RelACount = 0x6ffffff9,
+}
+impl_parse_for_enum!(DynamicTag, le_u64);
+
+#[derive(Debug)]
+pub struct DynamicEntry {
+  pub tag: DynamicTag,
+  pub addr: Addr,
+}
+
+impl DynamicEntry {
+  fn parse(i: parse::Input) -> parse::Result<Self> {
+      let (i, (tag, addr)) = tuple((DynamicTag::parse, Addr::parse))(i)?;
+      Ok((i, Self { tag, addr }))
+  }
+}
+
+#[derive(Debug)]
+pub enum SegmentContents {
+  Dynamic(Vec<DynamicEntry>),
+  Unknown,
+}
 
 pub struct ProgramHeader {
   pub r#type: SegmentType,
@@ -154,6 +308,7 @@ pub struct ProgramHeader {
   pub memsz: Addr,
   pub align: Addr,
   pub data: Vec<u8>,
+  pub contents: SegmentContents,
 }
 
 use std::ops::Range;
@@ -173,13 +328,34 @@ impl ProgramHeader {
     self.vaddr..self.vaddr + self.memsz
   }
   
-  fn parse<'a>(full_input: parse::Input<'_>, i: parse::Input<'a>) -> parse::Result<'a, Self> {
-    use nom::sequence::tuple;
+  fn parse<'a>(full_input: parse::Input<'a>, i: parse::Input<'a>) -> parse::Result<'a, Self> {
     let (i, (r#type, flags)) = tuple((SegmentType::parse, SegmentFlag::parse))(i)?;
     
     let ap = Addr::parse;
     let (i, (offset, vaddr, paddr, filesz, memsz, align)) = tuple((ap, ap, ap, ap, ap, ap))(i)?;
     
+    use nom::{combinator::{map, verify}, multi::many_till};
+    
+    // this used to be directly in the Self struct literal, but
+    // we're going to use it in the next block to parse dynamic entries from it.
+    let slice = &full_input[offset.into()..][..filesz.into()];
+    let (_, contents) = match r#type {
+      SegmentType::Dynamic => {
+        // *if* this is a Dynamic segment, we parse its contents. we haven't
+        // implemented `DynamicEntry::parse` yet, but it's coming!
+        let entry_size = 16_usize;
+        let n = slice.len()  / entry_size;
+        map(
+          many_till(
+            DynamicEntry::parse,
+            verify(DynamicEntry::parse, |e| e.tag == DynamicTag::Null),
+          ),
+         |(entries, _last)| SegmentContents::Dynamic(entries),
+        )(slice)?
+      }
+      _ => (slice, SegmentContents::Unknown),
+    };
+
     let res = Self {
       r#type,
       flags,
@@ -190,7 +366,8 @@ impl ProgramHeader {
       memsz,
       align,
       // `to_vec()` turns a slice into an owned Vec (this works because u8 is Clone+Copy)
-      data: full_input[offset.into()..][..filesz.into()].to_vec(),
+      data: slice.to_vec(),
+      contents,
     };
     Ok((i, res))
   }
--- elk/src/main.rs
+++ elk/src/main.rs
@@ -9,147 +9,200 @@ fn main() -> Result<(), Box<dyn Error>> {
   let input_path = env::args().nth(1).expect("usage: elk FILE");
   let input = fs::read(&input_path)?;
   
-  println!("Analyzing {}...", input_path);
+  println!("Analyzing {:?}...", input_path);
   let file = match delf::File::parse_or_print_error(&input[..]) {
     Some(f) => f,
     None => std::process::exit(1),
   };
   println!("{:#?}", file);
-
+  
   // println!("Executing {:?}...", input_path);
   // use std::process::Command;
   // let status = Command::new(input_path).status()?;
   // if !status.success() {
-  //   return Err("process did not exit successfully".into());
-  // }
-
-  // println!("Disassembling {:?}...", input_path);
-  let code_ph = file
-      .program_headers
-      .iter()
-      .find(|ph| ph.mem_range().contains(&file.entry_point))
-      .expect("segment with entry point not found");
-
-  // ndisasm(&code_ph.data[..], file.entry_point)?;
-
-
-  let base = 0x400000_usize;
-
-  println!("Mapping {:?} into memory...", input_path);
-  // we'll need to hold onto our "mmap::MemoryMap", because dropping them
-  // unmaps the memory
-  let mut mappings = Vec::new();
-
-  // we're only interested in "Load" segments
-  for ph in file
+    //   return Err("process did not exit successfully".into());
+    // }
+    
+    // println!("Disassembling {:?}...", input_path);
+    let code_ph = file
+    .program_headers
+    .iter()
+    .find(|ph| ph.mem_range().contains(&file.entry_point))
+    .expect("segment with entry point not found");
+    
+    // ndisasm(&code_ph.data[..], file.entry_point)?;
+    
+    println!("Dynamic entries:");
+    if let Some(ds) = file
+    .program_headers
+    .iter()
+    .find(|ph| ph.r#type == delf::SegmentType::Dynamic)
+    {
+      if let delf::SegmentContents::Dynamic(ref table) = ds.contents {
+        for entry in table {
+          println!("-  {:?}", entry);
+        }
+      }
+    }
+    
+    println!("Rela entries:");
+    let rela_entries = file.read_rela_entries()?;
+    for e in &rela_entries {
+      println!("{:#?}", e);
+      if let Some(seg) = file.segment_at(e.offset) {
+        println!("... for {:#?}", seg);
+      }
+    }
+    
+    let base = 0x400000_usize;
+    
+    println!("Mapping {:?} into memory...", input_path);
+    // we'll need to hold onto our "mmap::MemoryMap", because dropping them
+    // unmaps the memory
+    let mut mappings = Vec::new();
+    
+    // we're only interested in "Load" segments
+    for ph in file
     .program_headers
     .iter()
     .filter(|ph| ph.r#type == delf::SegmentType::Load)
     .filter(|ph| ph.mem_range().end > ph.mem_range().start)
-  {
-    let mem_range = ph.mem_range();
-    println!("Mapping segments @ {:?} with {:?}", mem_range, ph.flags);
-    // note: mmap-ing would fail if the segments weren't aligned on page boundaries,
-    // but luckily, that is the case in the file already. That is not a coincidence.
-    let len: usize = (mem_range.end - mem_range.start).into();
-    
-    // add base offset to the program header's virtual address
-    let start: usize = mem_range.start.0 as usize + base;
-    let aligned_start: usize = align_lo(start);
-    let padding = start - aligned_start;
-    let len = len + padding;
-
-    // `as` is the "cast" operator, and `_` is a placeholder to force rustc
-    // to infer the tpe based on other hints
-    let addr: *mut u8 = aligned_start as _;
-    println!("Addr: {:p}, Padding: {:08x}", addr, padding);
-
-    // at first, we want the memory area to be writable, so we can copy to it
-    let map = MemoryMap::new(len, &[MapOption::MapWritable, MapOption::MapAddr(addr)])?;
-  
-    println!("Copying segment data...");
     {
-      let dst = unsafe { std::slice::from_raw_parts_mut(addr.add(padding), ph.data.len()) };
-      dst.copy_from_slice(&ph.data[..]);
-    }
-
-    println!("Adjusting memory protection...");
-    // the `region` crate and our `delf` crate have two different
-    // enums (and bit flags) for protection, so we need to map from
-    // delf's to region's
-    let mut protection = Protection::NONE;
-    for flag in ph.flags.iter() {
-      protection |= match flag {
-        delf::SegmentFlag::Read => Protection::READ,
-        delf::SegmentFlag::Write => Protection::WRITE,
-        delf::SegmentFlag::Execute => Protection::EXECUTE,
+      let mem_range = ph.mem_range();
+      println!("Mapping segments @ {:?} with {:?}", mem_range, ph.flags);
+      // note: mmap-ing would fail if the segments weren't aligned on page boundaries,
+      // but luckily, that is the case in the file already. That is not a coincidence.
+      let len: usize = (mem_range.end - mem_range.start).into();
+      
+      // add base offset to the program header's virtual address
+      let start: usize = mem_range.start.0 as usize + base;
+      let aligned_start: usize = align_lo(start);
+      let padding = start - aligned_start;
+      let len = len + padding;
+      
+      // `as` is the "cast" operator, and `_` is a placeholder to force rustc
+      // to infer the tpe based on other hints
+      let addr: *mut u8 = aligned_start as _;
+      println!("Addr: {:p}, Padding: {:08x}", addr, padding);
+      
+      // at first, we want the memory area to be writable, so we can copy to it
+      let map = MemoryMap::new(len, &[MapOption::MapWritable, MapOption::MapAddr(addr)])?;
+      
+      println!("Copying segment data...");
+      {
+        let dst = unsafe { std::slice::from_raw_parts_mut(addr.add(padding), ph.data.len()) };
+        dst.copy_from_slice(&ph.data[..]);
       }
+      
+      println!("Applying relocations (if any)...");
+      for reloc in &rela_entries {
+        if mem_range.contains(&reloc.offset) {
+          unsafe {
+            use std::mem::transmute as trans;
+            let real_segment_start = addr.add(padding);
+            
+            let specified_reloc_offset = reloc.offset;
+            let specified_segment_start = mem_range.start;
+            let offset_into_segment = specified_reloc_offset - specified_segment_start;
+            
+            println!(
+              "Applying {:?} relocation @ {:?} from segment start",
+              reloc.r#type, offset_into_segment
+            );
+            
+            let reloc_addr: *mut u64 =
+            trans(real_segment_start.add(offset_into_segment.into()));
+            match reloc.r#type {
+              delf::RelType::Relative => {
+                let reloc_value = reloc.addend + delf::Addr(base as u64);
+                println!("Replacing with value {:?}", reloc_value);
+                *reloc_addr = reloc_value.0;
+              }
+              r#type => {
+                panic!("Unsupported relocation type {:?}", r#type);
+              }
+            }
+          }
+        }
+      }
+      
+      println!("Adjusting memory protection...");
+      // the `region` crate and our `delf` crate have two different
+      // enums (and bit flags) for protection, so we need to map from
+      // delf's to region's
+      let mut protection = Protection::NONE;
+      for flag in ph.flags.iter() {
+        protection |= match flag {
+          delf::SegmentFlag::Read => Protection::READ,
+          delf::SegmentFlag::Write => Protection::WRITE,
+          delf::SegmentFlag::Execute => Protection::EXECUTE,
+        }
+      }
+      
+      unsafe {
+        protect(addr, len, protection)?;
+      }
+      mappings.push(map);
     }
-
+    
+    println!("\nExecuting {:?} in memory...", input_path);
+    let code = &code_ph.data[..];
     unsafe {
-      protect(addr, len, protection)?;
+      protect(code.as_ptr(), code.len(), Protection::READ_WRITE_EXECUTE)?;
     }
-    mappings.push(map);
+    
+    let entry_offset = file.entry_point - code_ph.mem_range().start;
+    let entry_point = unsafe { code.as_ptr().add(entry_offset.into()) };
+    println!("       code  @ {:?}", code.as_ptr());
+    println!("entry offset @ {:?}", entry_offset);
+    println!("entry point  @ {:?}\n", entry_point);
+    
+    println!("Jumping to entry point @ {:?}...", file.entry_point);
+    pause("jmp")?;
+    unsafe {
+      jmp((file.entry_point.0 as usize + base) as _);
+    }
+    
+    Ok(())
   }
-
-  println!("\nExecuting {:?} in memory...", input_path);
-  let code = &code_ph.data[..];
-  unsafe {
-    protect(code.as_ptr(), code.len(), Protection::READ_WRITE_EXECUTE)?;
-  }
-
-  let entry_offset = file.entry_point - code_ph.mem_range().start;
-  let entry_point = unsafe { code.as_ptr().add(entry_offset.into()) };
-  println!("       code  @ {:?}", code.as_ptr());
-  println!("entry offset @ {:?}", entry_offset);
-  println!("entry point  @ {:?}\n", entry_point);
-
-  println!("Jumping to entry point @ {:?}...", file.entry_point);
-  pause("jmp")?;
-  unsafe {
-    jmp((file.entry_point.0 as usize + base) as _);
-  }
-
-  Ok(())
-}
-
-fn ndisasm(code: &[u8], origin: delf::Addr) -> Result<(), Box<dyn Error>> {
-  use std::{
-    io::Write,
-    process::{Command, Stdio},
-  };
-
-  let mut child = Command::new("ndisasm")
-      .arg("-b")
-      .arg("64")
-      .arg("-")
-      .stdin(Stdio::piped())
-      .stdout(Stdio::piped())
-      .spawn()?;
+  
+  fn ndisasm(code: &[u8], origin: delf::Addr) -> Result<(), Box<dyn Error>> {
+    use std::{
+      io::Write,
+      process::{Command, Stdio},
+    };
+    
+    let mut child = Command::new("ndisasm")
+    .arg("-b")
+    .arg("64")
+    .arg("-")
+    .stdin(Stdio::piped())
+    .stdout(Stdio::piped())
+    .spawn()?;
     child.stdin.as_mut().unwrap().write_all(code)?;
-  let output = child.wait_with_output()?;
-  println!("{}", String::from_utf8_lossy(&output.stdout));
-
-  Ok(())
-}
-
-unsafe fn jmp(addr: *const u8) {
-  let fn_ptr: fn() = std::mem::transmute(addr);
-  fn_ptr();
-}
-
-fn pause(reason: &str) -> Result<(), Box<dyn Error>> {
-  println!("Press Enter to {}...", reason);
-  {
+    let output = child.wait_with_output()?;
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+    
+    Ok(())
+  }
+  
+  unsafe fn jmp(addr: *const u8) {
+    let fn_ptr: fn() = std::mem::transmute(addr);
+    fn_ptr();
+  }
+  
+  fn pause(reason: &str) -> Result<(), Box<dyn Error>> {
+    println!("Press Enter to {}...", reason);
+    {
       let mut s = String::new();
       std::io::stdin().read_line(&mut s)?;
+    }
+    Ok(())
   }
-  Ok(())
-}
-
-/**
- * Truncates a usize value to the left-adjacent (low) 4KiB boundary.
- */
-fn align_lo(x: usize) -> usize {
-  x & !0xFFF
-}
+  
+  /**
+  * Truncates a usize value to the left-adjacent (low) 4KiB boundary.
+  */
+  fn align_lo(x: usize) -> usize {
+    x & !0xFFF
+  }
